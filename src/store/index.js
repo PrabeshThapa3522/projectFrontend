d02d1244fcdd0820f048268ae965b6182d0eb8c0
PrabeshThapa3522 <thapa.pranish7835@gmail.com>
2024-12-09 23:04:22 +0545

    Last Commit

---
+++ src/store/index.js
@@ -0,0 +1,82 @@
+/*import { configureStore, createSlice } from "@reduxjs/toolkit";
+const userSclice = createSlice({
+    name: "user",
+    initialState: { isLoggedIn: false },
+    reducers: {
+      login(state) {
+        state.isLoggedIn = true;
+      },
+      logout(state) {
+        localStorage.removeItem("userId");
+        state.isLoggedIn = false;
+      },
+    },
+  });
+
+  const adminSlice = createSlice({
+    name: "auth",
+    initialState: { isLoggedIn: false },
+    reducers: {
+      login(state) {
+        state.isLoggedIn = true;
+      },
+      logout(state) {
+        localStorage.removeItem("adminId");
+        localStorage.removeItem("token");
+        state.isLoggedIn = false;
+      },
+    },
+  });
+    
+  
+  export const userActions = userSclice.actions;
+  export const adminActions = adminSlice.actions;
+
+  export const store = configureStore({
+    reducer: {
+      user: userSclice.reducer,
+      admin: adminSlice.reducer,
+    },
+  });*/
+
+
+  import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: { isLoggedIn: false },
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      localStorage.removeItem("userId");
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+const adminSlice = createSlice({
+  name: "admin", // Updated for clarity
+  initialState: { isLoggedIn: false },
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      localStorage.removeItem("adminId");
+      localStorage.removeItem("token");
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const userActions = userSlice.actions;
+export const adminActions = adminSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    user: userSlice.reducer,
+    admin: adminSlice.reducer,
+  },
+});
